a5f69bcaeac2dce809061c2f4231cb09eb24c3a8
Pj <ngpinjie1999@gmail.com>
2023-10-20 17:41:14 +0800

    QuadTree implementation 1
    Average compressed size - 54k bytes
    Running Time - 31.5 ms
    Loss:
    MAE - 29.26
    MSE - 8481.89
    PSNR - 9.86

--- Utility.java
+++ Utility.java
@@ -2,28 +2,209 @@ import java.io.*;
 
 public class Utility {
 
-    public void Compress(int[][][] pixels, String outputFileName) throws IOException {
-        // The following is a bad implementation that we have intentionally put in the function to make App.java run, you should 
-        // write code to reimplement the function without changing any of the input parameters, and making sure the compressed file
-        // gets written into outputFileName
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) { 
-            oos.writeObject(pixels); 
+    private static final int THRESHOLD = 50;
+
+    private QuadtreeNode buildQuadtree(int[][][] pixels, int x, int y, int size) {
+        if (x < 0 || y < 0 || x >= pixels.length || y >= pixels[0].length || size <= 0) {
+            return null;
         }
+        
+        QuadtreeNode node = new QuadtreeNode(x, y, size);
+    
+        // Base case: if size is 1, then create a leaf node with the color of that pixel.
+        if (size == 1) {
+            node.color = pixels[x][y];
+            return node;
+        }
+        
+        int[] avgColor = calculateAverageColor(pixels, x, y, size);
+        if (colorDifferenceWithinThreshold(pixels, x, y, size, avgColor)) {
+            node.color = avgColor;
+        } else {
+            int newSize = size / 2;
+            node.children = new QuadtreeNode[4];
+            node.children[0] = buildQuadtree(pixels, x, y, newSize); // top-left quadrant
+            node.children[1] = buildQuadtree(pixels, x + newSize, y, newSize); // top-right quadrant
+            node.children[2] = buildQuadtree(pixels, x, y + newSize, newSize); // bottom-left quadrant
+            node.children[3] = buildQuadtree(pixels, x + newSize, y + newSize, newSize); // bottom-right quadrant
+        }
+    
+        return node;
     }
 
-    public int[][][] Decompress(String inputFileName) throws IOException, ClassNotFoundException {
-        // The following is a bad implementation that we have intentionally put in the function to make App.java run, you should 
-        // write code to reimplement the function without changing any of the input parameters, and making sure that it returns
-        // an int [][][]
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFileName))) {
-            Object object = ois.readObject();
+    private int[] calculateAverageColor(int[][][] pixels, int x, int y, int size) {
+        long sumR = 0, sumG = 0, sumB = 0;
+        int maxX = Math.min(x + size, pixels.length);
+        int maxY = Math.min(y + size, pixels[0].length);
     
-            if (object instanceof int[][][]) {
-                return (int[][][]) object;
-            } else {
-                throw new IOException("Invalid object type in the input file");
+        for (int i = x; i < maxX; i++) {
+            for (int j = y; j < maxY; j++) {
+                sumR += pixels[i][j][0];
+                sumG += pixels[i][j][1];
+                sumB += pixels[i][j][2];
+            }
+        }
+    
+        int totalPixels = (maxX - x) * (maxY - y);
+        if (totalPixels == 0) {
+            return new int[]{0, 0, 0}; // Return black if no pixels to average
+        }
+        return new int[]{(int) (sumR / totalPixels), (int) (sumG / totalPixels), (int) (sumB / totalPixels)};        
+    }
+
+    private boolean colorDifferenceWithinThreshold(int[][][] pixels, int x, int y, int size, int[] averageColor) {
+        if (x < 0 || y < 0 || x >= pixels.length || y >= pixels[0].length) {
+            return false;
+        }
+        
+        int red = averageColor[0];
+        int green = averageColor[1];
+        int blue = averageColor[2];
+        
+        int maxX = Math.min(x + size, pixels.length);
+        int maxY = Math.min(y + size, pixels[0].length);
+    
+        int countExceedingThreshold = 0;
+        int allowedExceedingThreshold = (int) (0.1 * size * size); // Allow up to 10% of the pixels to exceed the threshold
+        
+        for (int i = x; i < maxX; i++) {
+            for (int j = y; j < maxY; j++) {
+                int diffRed = Math.abs(pixels[i][j][0] - red);
+                int diffGreen = Math.abs(pixels[i][j][1] - green);
+                int diffBlue = Math.abs(pixels[i][j][2] - blue);
+                
+                if (diffRed > THRESHOLD || diffGreen > THRESHOLD || diffBlue > THRESHOLD) {
+                    countExceedingThreshold++;
+                    if (countExceedingThreshold > allowedExceedingThreshold) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    public void Compress(int[][][] pixels, String outputFileName) throws IOException {
+        if(pixels == null || pixels.length == 0 || pixels[0].length == 0) {
+            throw new IllegalArgumentException("Invalid pixel data provided.");
+        }
+    
+        int maxSize = Math.max(pixels.length, pixels[0].length); // Handle non-square images
+        QuadtreeNode root = buildQuadtree(pixels, 0, 0, maxSize);
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) {
+            oos.writeInt(maxSize);  // Write the image size
+            writeQuadtree(oos, root);
+        }
+    }
+    
+    
+    private void writeQuadtree(ObjectOutputStream oos, QuadtreeNode node) throws IOException {
+        if (node == null) {
+            oos.writeBoolean(false);
+            return;
+        }
+        
+        oos.writeBoolean(true);
+
+        if (node.color != null) {
+            oos.writeBoolean(true);
+            oos.writeInt(colorToInt(node.color)); // Using our optimized color representation
+        } else {
+            oos.writeBoolean(false);
+            for (int i = 0; i < 4; i++) {
+                writeQuadtree(oos, node.children[i]);
+            }
+        }
+    }
+    
+    private int colorToInt(int[] color) {
+        return (color[0] << 16) | (color[1] << 8) | color[2];
+    }
+
+    private int[] intToColor(int rgb) {
+        return new int[]{(rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF};
+    }
+    
+    public int[][][] Decompress(String inputFileName) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFileName))) {
+            int size = ois.readInt();
+            QuadtreeNode root = readQuadtree(ois, 0, 0, size);
+            int[][][] pixels = new int[size][size][3];
+            reconstructImage(pixels, root);
+    
+            if(pixels.length == 0 || pixels[0].length == 0) {
+                throw new IllegalArgumentException("Decompression resulted in invalid pixel data.");
+            }
+    
+            return pixels;
+        }
+    }
+    
+    private QuadtreeNode readQuadtree(ObjectInputStream ois, int x, int y, int size) throws IOException {
+        boolean exists = ois.readBoolean();
+        
+        if (!exists) {
+            return null;
+        }
+        
+        boolean isLeaf = ois.readBoolean();
+    
+        QuadtreeNode node = new QuadtreeNode(x, y, size);
+        
+        if (isLeaf) {
+            node.color = intToColor(ois.readInt());
+        } else {
+            int newSize = size / 2;
+            node.children = new QuadtreeNode[4];
+            node.children[0] = readQuadtree(ois, x, y, newSize);
+            node.children[1] = readQuadtree(ois, x + newSize, y, newSize);
+            node.children[2] = readQuadtree(ois, x, y + newSize, newSize);
+            node.children[3] = readQuadtree(ois, x + newSize, y + newSize, newSize);
+        }
+        return node;
+    }
+    
+    private void reconstructImage(int[][][] pixels, QuadtreeNode node) {
+        if (node == null) {
+            return;  // Return immediately if the node is null
+        }
+        
+        if (node.color != null) {
+            for (int i = node.x; i < node.x + node.size && i < pixels.length; i++) {
+                for (int j = node.y; j < node.y + node.size && j < pixels[i].length; j++) {
+                    pixels[i][j] = node.color;
+                }
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                reconstructImage(pixels, node.children[i]);
             }
         }
     }
 
 }
+
+// public void Compress(int[][][] pixels, String outputFileName) throws IOException {
+//     // The following is a bad implementation that we have intentionally put in the function to make App.java run, you should 
+//     // write code to reimplement the function without changing any of the input parameters, and making sure the compressed file
+//     // gets written into outputFileName
+//     try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) { 
+//         oos.writeObject(pixels); 
+//     }
+// }
+
+// public int[][][] Decompress(String inputFileName) throws IOException, ClassNotFoundException {
+//     // The following is a bad implementation that we have intentionally put in the function to make App.java run, you should 
+//     // write code to reimplement the function without changing any of the input parameters, and making sure that it returns
+//     // an int [][][]
+//     try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFileName))) {
+//         Object object = ois.readObject();
+
+//         if (object instanceof int[][][]) {
+//             return (int[][][]) object;
+//         } else {
+//             throw new IOException("Invalid object type in the input file");
+//         }
+//     }
+// }
