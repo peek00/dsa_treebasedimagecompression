df4e7d7b72b090cea0f5ec035eb2a69ff13727bd
Pj <ngpinjie1999@gmail.com>
2023-10-23 15:58:52 +0800

    update testframework

---
+++ QuadtreeNode.java
@@ -0,0 +1,14 @@
+public class QuadtreeNode {
+    int x, y; // top-left corner of this node
+    int size; // width/height of this node
+    int[] color; // average color of this node (if it's a leaf)
+    QuadtreeNode[] children; // children of this node (if it's not a leaf)
+
+    public QuadtreeNode(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = null;
+        this.children = null;
+    }
+}
