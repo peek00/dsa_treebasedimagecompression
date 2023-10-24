009a4fa877cfae75560373938cb4920cf6c25fc5
potaytoh2 <axelwong153@gmail.com>
2023-10-24 10:26:05 +0800

    done yay!

--- Utility.java
+++ Utility.java
@@ -46,26 +46,26 @@ class Node{
         
 
         if(width/2 <= 2 || height /2 <= 2){
-            // avgColor = averageColor(x, y, width, height, pixels);
-            avgColor=new int[3];
-            avgColor[0]=255;
-            avgColor[1]=255;
-            avgColor[2]=255;
+            avgColor = averageColor(x, y, width, height, pixels);
+            // avgColor=new int[3];
+            // avgColor[0]=255;
+            // avgColor[1]=255;
+            // avgColor[2]=255;
             isLeaf = true;
         }
         else if(measureDetail(x,y,width,height,pixels)<threshold){
-            // avgColor = averageColor(x, y, width, height, pixels);
-            avgColor=new int[3];
-            avgColor[0]=255;
-            avgColor[1]=255;
-            avgColor[2]=255;
+            avgColor = averageColor(x, y, width, height, pixels);
+            // avgColor=new int[3];
+            // avgColor[0]=255;
+            // avgColor[1]=255;
+            // avgColor[2]=255;
             isLeaf = true;
         }
         else{
             double width_divided = Math.round((double) width/2);
             double height_divided = Math.round((double) height/2);
-            System.out.println("width_divided: "+width_divided+" height_divided: "+height_divided);
-            System.out.println("_________________________________");
+            // System.out.println("width_divided: "+width_divided+" height_divided: "+height_divided);
+            // System.out.println("_________________________________");
             int widthConverted = (int) width_divided;
             int heightConverted = (int) height_divided;
             children = new Node[4];
@@ -74,22 +74,39 @@ class Node{
             // upper right quadrant or north-east
             children[1] = new Node(x+widthConverted,y,widthConverted,heightConverted,pixels,threshold);
             // lower left quadrant or south-west
-            children[2] = new Node(x,y+heightConverted,width/2,height-heightConverted,pixels,threshold);
+            children[2] = new Node(x,y+heightConverted,widthConverted,height-heightConverted,pixels,threshold);
             // lower right quadrant or south-east
             children[3] = new Node(x+widthConverted,y+heightConverted,width-widthConverted,height-heightConverted,pixels,threshold);
         }
 	}
 
     public int[] averageColor(int x, int y, int width, int height, int[][][] pixels){
-        System.out.println("x: "+x+" y: "+y+" width: "+width+" height: "+height);
+        // System.out.println("x: "+x+" y: "+y+" width: "+width+" height: "+height);
         int red = 0;
         int green = 0;
         int blue = 0;
         for(int i = x; i < x + width; i++){
             for(int j = y; j < y + height; j++){
-                red += pixels[i][j][0];
-                green += pixels[i][j][1];
-                blue += pixels[i][j][2];
+                if(i>=pixels.length && j>=pixels[0].length){
+                    red+=pixels[pixels.length-1][pixels[0].length-1][0];
+                    green+=pixels[pixels.length-1][pixels[0].length-1][1];
+                    blue+=pixels[pixels.length-1][pixels[0].length-1][2];
+
+                }else if(i>=pixels.length){
+                    red+=pixels[pixels.length-1][j][0];
+                    green+=pixels[pixels.length-1][j][1];
+                    blue+=pixels[pixels.length-1][j][2];
+
+                }else if(j>=pixels[0].length){
+                    red+=pixels[i][pixels[0].length-1][0];
+                    green+=pixels[i][pixels[0].length-1][1];
+                    blue+=pixels[i][pixels[0].length-1][2];
+    
+                }else{
+                    red += pixels[i][j][0];
+                    green += pixels[i][j][1];
+                    blue += pixels[i][j][2];
+                }
             }
         }
         //number of pixels evaluated
@@ -133,6 +150,22 @@ class Node{
          */
         for(int i = x; i < x + width; i++){
             for(int j = y; j < y + height; j++){
+                if(i>=pixels.length && j>=pixels[0].length){
+                    red+=pixels[i][pixels[0].length-1][0];
+                    green+=pixels[i][pixels[0].length-1][1];
+                    blue+=pixels[i][pixels[0].length-1][2];
+                    continue;
+                }else if(i>=pixels.length){
+                    red+=pixels[pixels.length-1][j][0];
+                    green+=pixels[pixels.length-1][j][1];
+                    blue+=pixels[pixels.length-1][j][2];
+                    continue;
+                }else if(j>=pixels[0].length){
+                    red+=pixels[i][pixels[0].length-1][0];
+                    green+=pixels[i][pixels[0].length-1][1];
+                    blue+=pixels[i][pixels[0].length-1][2];
+                    continue;
+                }
                 int pixelRed = pixels[i][j][0];
                 int pixelGreen = pixels[i][j][1];
                 int pixelBlue = pixels[i][j][2];
@@ -284,7 +317,7 @@ public class Utility {
         System.out.println("Image Width: " + width);
         System.out.println("Image Height: " + height);
 
-        Quadtree tree = new Quadtree(0,0,width,height,pixels,10000);
+        Quadtree tree = new Quadtree(0,0,width,height,pixels,50);
         byte[] binaryData = Quadtree.encodeQuadTree(tree.root);
         try(FileOutputStream fos = new FileOutputStream(outputFileName)){
             fos.write(binaryData);
@@ -308,7 +341,9 @@ public class Utility {
         if (node.children == null) {
             for (int i = node.x; i < node.x + node.width; i++) {
                 for (int j = node.y; j < node.y + node.height; j++) {
-                   
+                   if(i>=pixels.length || j>=pixels[0].length){
+                    continue;
+                }
                     pixels[i][j][0] = node.avgColor[0];
                     pixels[i][j][1] = node.avgColor[1];
                     pixels[i][j][2] = node.avgColor[2];
